54be58b1629831837ab1de75750101440ddb4049
anrajin <an.rajin@gmail.com>
2022-12-16 23:09:42 +0600

    post controller and view added

---
+++ src/Neon.Web/Areas/Admin/Controllers/PostsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Neon.Web.Areas.Admin.Controllers
+{
+    public class PostsController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create(string name)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpDelete, ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+    }
+}
