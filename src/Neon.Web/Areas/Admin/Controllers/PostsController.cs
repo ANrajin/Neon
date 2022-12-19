fca37e2ca0338af1fdbf85eb153c7898ead2ac7e
anrajin <an.rajin@gmail.com>
2022-12-19 22:50:10 +0600

    post create action added

--- src/Neon.Web/Areas/Admin/Controllers/PostsController.cs
+++ src/Neon.Web/Areas/Admin/Controllers/PostsController.cs
@@ -1,22 +1,49 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Autofac;
+using Microsoft.AspNetCore.Mvc;
+using Neon.Web.Areas.Admin.Models.PostModels;
 
 namespace Neon.Web.Areas.Admin.Controllers
 {
     public class PostsController : BaseController
     {
+        private readonly ILifetimeScope _scope;
+        private readonly ILogger<CategoriesController> _logger;
+
+        public PostsController(ILifetimeScope scope, ILogger<CategoriesController> logger)
+        {
+            _scope = scope;
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Create(string name)
+        public IActionResult Create()
         {
-            return View();
+            var model = new PostCreateModel();
+            _scope.Resolve<PostCreateModel>();
+            return View(model);
         }
 
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Create()
+        public IActionResult Create(PostCreateModel model)
         {
+            if (ModelState.IsValid)
+            {
+                model.Resolve(_scope);
+
+                try
+                {
+                    model.CreatePost();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    throw;
+                }
+            }
             return View();
         }
 
