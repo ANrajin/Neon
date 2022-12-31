981fb5671aa5db209a4f941160fc28c38aeefef5
anrajin <an.rajin@gmail.com>
2022-12-31 20:53:09 +0600

    role seeding

--- src/Neon.Web/Data/Migrations/20221231103028_Create_Role_seed.Designer.cs
+++ src/Neon.Web/Data/Migrations/20221231103028_Create_Role_seed.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Neon.Web.Data;
 
@@ -11,9 +12,10 @@ using Neon.Web.Data;
 namespace Neon.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221231103028_Create_Role_seed")]
+    partial class Create_Role_seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Neon.Web/Data/Migrations/20221231103028_Create_Role_seed.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Neon.Web.Data.Migrations
+{
+    public partial class Create_Role_seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1C57C706-662C-46E7-8240-839F1A9ACD0C", "e719ef30-2f80-4075-88c4-f60fe98c215b", "Author", "AUTHOR" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "CCD9FE14-EF0A-4B7F-9741-E7D968E0AB88", "3af6937b-5d29-4f18-8498-690b147ed17b", "Admin", "ADMIN" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1C57C706-662C-46E7-8240-839F1A9ACD0C");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "CCD9FE14-EF0A-4B7F-9741-E7D968E0AB88");
+        }
+    }
+}
